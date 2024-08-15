801d05d5a3d24bad6fd88e5a71262a65a0c43649
Satish Nayak <satish_nayak@epam.com>
2024-08-15 14:42:44 -0400

    baseline

---
+++ src/test/java/com/instanceofcake/vehicle_app/VehicleAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.instanceofcake.vehicle_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehicleAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
